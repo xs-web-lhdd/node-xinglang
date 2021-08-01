70ed07c6fa7cff50c404a30ffc1ac9258634b55d
凉风有信 <3042146237@qq.com>
2021-08-01 15:00:15 +0800

    feat:单元测试

---
+++ test/server.js
@@ -0,0 +1,11 @@
+/**
+ * @description jest servr
+ * @author 凉风有信、
+ */
+
+const request = require('supertest')
+
+const server = require('../src/app').callback()
+
+
+module.exports = request(server)
