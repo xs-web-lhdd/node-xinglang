6b3bbb0bec6e77fb347bccc45353c929ed0f23c3
凉风有信 <3042146237@qq.com>
2021-08-30 20:57:26 +0800

    feat:初始化

--- src/routes/view/blog.js
+++ src/routes/view/blog.js
@@ -6,8 +6,23 @@
 const router = require('koa-router')()
 const { loginRedirect } = require('../../middlewares/loginChecks')
 
+// 首页
 router.get('/', loginRedirect, async (ctx, next) => {
     await ctx.render('index', {})
+})
+
+// 个人主页
+router.get('/profile', loginRedirect, async (ctx, next) => {
+    const { userName } = ctx.session.userInfo
+    ctx.redirect(`/profile/${userName}`)
+})
+router.get('/profile/:userName', loginRedirect, async (ctx, next) => {
+    // 获取微博第一页数据
+    await ctx.render('profile', {
+        blogData: {
+
+        }
+    })
 })
 
 module.exports = router
