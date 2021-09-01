94da3c040bc790cb50943b30e20cc1cea1d84eaa
凉风有信 <3042146237@qq.com>
2021-09-01 10:38:28 +0800

    feat:首页加载更多（跟广场页一样）

--- src/routes/api/blog-home.js
+++ src/routes/api/blog-home.js
@@ -8,6 +8,10 @@ const { loginCheck } = require('../../middlewares/loginChecks')
 const { create } = require('../../controller/blog-home')
 const { genValidator } = require('../../middlewares/validator')
 const { blogValidate } = require('../../validator/blog')
+// 加载更多
+const { getSquareBlogList } = require('../../controller/blog-square')
+const { getBlogListString } = require('../../untils/blog')
+
 router.prefix('/api/blog')
 
 // 创建微博
@@ -15,6 +19,17 @@ router.post('/create', loginCheck, genValidator(blogValidate), async (ctx, next)
     const { content, image } = ctx.request.body
     const { id: userId } = ctx.session.userInfo
     ctx.body = await create({content, userId, image})
+})
+
+// 加载更多
+router.get('/loadMore/:pageIndex', loginCheck, async (ctx, next) => {
+    let { pageIndex } = ctx.params
+    pageIndex = parseInt(pageIndex)
+    const result = await getSquareBlogList(pageIndex)
+    // 渲染为html字符串
+    result.data.blogListTpl = getBlogListString(result.data.blogList)
+    // 返回
+    ctx.body = result
 })
 
 
